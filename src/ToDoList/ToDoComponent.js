8b2916f6842fe4bb14b8cfba26ecf8f93353d397
Kanagaraj Kandasamy <Kanagaraj.Kandasamy@xome.com>
2019-07-29 23:00:39 +0530

    ToDoList Empty check added.

--- src/ToDoList/ToDoComponent.js
+++ src/ToDoList/ToDoComponent.js
@@ -1,24 +1,39 @@
 import React, {Component} from 'react'
 import ToDoListComponent from './ToDoListComponent'
+import {compose} from 'recompose'
 
 class ToDoComponent extends Component {
     constructor(props) {
         super(props)
-        this.todo = ["test", "best", "waste", "one"]
+        this.todo = ["one","two","three", "four", "five"]
     }
 
     nullcheck = (WraperComponent) => props =>       
-            props.todo === null 
+            !props.todo 
             ? null
             : <WraperComponent {...props}/>
+
+    emptyCheck =  EmptyComponent => ToDoComponent => props =>
+        props.todo && !props.todo.length
+        ? <EmptyComponent {...props}/>
+        : <ToDoComponent {...props}/>
+
+    emptyComponent = () => <div>There is no ToDoList Available.</div>
 
     render() {
 
         const props = Object.assign({}, this.props, {
           todo: this.todo   
         })
-        const ToDoComp = this.nullcheck(ToDoListComponent)
-        return (<ToDoComp {...props}/>)
+        const WithConditionalRender = compose(
+            this.emptyCheck(this.emptyComponent),
+            this.nullcheck 
+        )
+        const ConditionalRender = WithConditionalRender(ToDoListComponent)
+        //const ToDoComp = this.nullcheck(ToDoListComponent)
+        //const SecValidation = this.emptyCheck(ToDoComp, this.emptyComponent)
+
+        return (<ConditionalRender {...props}/>)
     }
 }
 
